cc49a99d973e6b9ad80f00d948526241ee804ed4
Dmitriy Bogomolov <dimones951@yandex.ru>
2021-12-04 22:26:48 +0700

    update curl

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-10-20 01:00:46 +0000
 import PackageDescription
 let package = Package(
 	name: "Perfect-GoogleAnalytics-MeasurementProtocol",
+	platforms: [.macOS(.v10_15)],
 	products: [
 		.library(name: "Perfect-GoogleAnalytics-MeasurementProtocol", targets: ["Perfect-GoogleAnalytics-MeasurementProtocol"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", .exact("5.0.0")),
 	],
 	targets: [
 		.target(name: "Perfect-GoogleAnalytics-MeasurementProtocol", dependencies: ["PerfectCURL"])
